e997460ba1eb3ba5967f63169028a3b35ad25c74
Sanjiv Jha <sanjiv@joshsoftware.com>
2015-05-21 13:44:11 +0530

    added name in package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 
+gulp.task('build:css', function name(){
+  console.log('running a build css');
+});
+
 gulp.task('default', function(){
   console.log('running default task');
-
-})
+});
