69247106dc204d828a74a93812b0141422020ca7
Sanjiv Jha <sanjiv@joshsoftware.com>
2015-05-21 14:18:57 +0530

    Resolve .gitignore commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,16 @@
 var gulp = require('gulp');
-
-gulp.task('build:css', function name(){
+var less = require('gulp-less');
+var concat = require('gulp-concat');
+gulp.task('build:css', function(){
+  gulp.src('src/css/*.less')
+  .pipe(less())
+  .pipe(concat('application.css'))
+  .pipe(gulp.dest('./app/assets/stylesheets'))
   console.log('running a build css');
+});
+
+gulp.task('build:js', function(){
+  gulp.src("src/js/*.js")
 });
 
 gulp.task('default', function(){
